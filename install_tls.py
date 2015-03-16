2abf5be7bf5eb47aae247feef9fe6f94f2eb10c7
taeguk <taeguk@CNUbuntuSrv.(none)>
2015-03-17 02:09:33 +0900

    commit

---
+++ install_tls.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import hashlib
+import getpass
+import os
+
+print "\n\n\n\n\n\n\n\n\n\t\t Terminal Lock System INSTALL\n\n\n\n"
+
+print "[*] Setting Password for authentication"
+pw = getpass.getpass("Input Password >> ")
+pw2 = getpass.getpass("Input Password (Re-typing) >> ")
+
+if pw != pw2:
+	print "[!] Fail to set password! (not match)"
+	exit()
+
+print "\n[*] Making tls.sh..."
+
+content_front = """
+#!/bin/bash
+clear
+trap "echo -ne \\"\\\\n[ Terminal Lock System ] Don't hack!!\\\\nInput Password >> \\"" {1..100} 2> /dev/null
+echo -e "\\n\\n\\n\\n\\n\\n\\n\\n\\n\\t\\t\\t\\t Terminal Lock System\\n\\n\\n\\n"
+echo -n "Input Password >> "
+read -s pass
+while [ "`echo -n $pass | md5sum | awk '{print $1}'`" != "
+"""
+content_behind = """
+" ]
+do
+	echo -e "\\n[ Terminal Lock System ] Wrong Password!!"
+	echo -n "Input Password >> "
+	read -s pass
+done
+clear
+"""
+content_center = hashlib.md5(pw).hexdigest()
+
+content = content_front.strip() + content_center.strip() + content_behind.strip()
+
+f = open("tls.sh","w")
+f.write(content)
+f.close()
+
+os.chmod("tls.sh", 0755)
+
+print "\n[*] All progress was finished!!"
+print "\n[*] [Usage] ./tls.sh\n"
